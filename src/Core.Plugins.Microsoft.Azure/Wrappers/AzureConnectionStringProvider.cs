febef7b60469f02f53c400c61c15076706194383
Scott Ferguson <scott.ferguson82@gmail.com>
2019-10-08 16:51:04 -0500

    Adding AzureConnectionStringProvider

---
+++ src/Core.Plugins.Microsoft.Azure/Wrappers/AzureConnectionStringProvider.cs
@@ -0,0 +1,33 @@
+﻿using Core.Exceptions;
+using Core.Providers;
+using Microsoft.Extensions.Configuration;
+
+namespace Core.Plugins.Microsoft.Azure.Wrappers
+{
+    public class AzureConnectionStringProvider : IConnectionStringProvider
+    {
+        private readonly IConfiguration _configuration;
+
+        public AzureConnectionStringProvider(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string Get(string connectionName)
+        {
+            string connectionString = _configuration.GetConnectionString(connectionName);
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new CoreException("Connection String cannot be null or empty");
+            }
+
+            return connectionString;
+        }
+
+        public string Get()
+        {
+            return Get("DefaultConnection");
+        }
+    }
+}
