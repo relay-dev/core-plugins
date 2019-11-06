693d1c061031fb0b617ac187e5e293b1a436669f
Scott Ferguson <scott.ferguson82@gmail.com>
2019-11-06 13:25:51 -0600

    Adding Core.Plugins.Microservices

---
+++ src/Core.Plugins.Microservices/Exceptions/MicroserviceException.cs
@@ -0,0 +1,16 @@
+﻿using Core.Exceptions;
+using System;
+
+namespace Core.Plugins.Microservices.Exceptions
+{
+    public class MicroserviceException : CoreException
+    {
+        public MicroserviceException(string message) : base(message) { }
+
+        public MicroserviceException(string errorCode, string message) : base(errorCode, message) { }
+
+        public MicroserviceException(Exception innerException, string message) : base(innerException, message) { }
+
+        public MicroserviceException(Exception innerException, string errorCode, string message) : base(innerException, errorCode, message) { }
+    }
+}
