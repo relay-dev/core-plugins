4f7f0d434bbeb86a9a1befc51b6a7407f98cc4dd
Scott Ferguson <scott.ferguson82@gmail.com>
2019-12-19 20:49:17 -0600

    Adding ProcessorAttribute

---
+++ src/Core.Plugins/Utilities/Processor/ProcessorAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Core.Plugins.Utilities.Processor
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Interface, AllowMultiple = false)]
+    public sealed class ProcessorAttribute : Attribute
+    {
+        /// <summary>
+        /// Specifies the name of the process the class represents
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
