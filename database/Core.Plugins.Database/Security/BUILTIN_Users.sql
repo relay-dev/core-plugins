470ece843da401ccddf52a9d8c6ceb69cc634c9c
Scott Ferguson <scott.ferguson82@gmail.com>
2020-02-16 12:44:12 -0600

    Removing database project

--- database/Core.Plugins.Database/Security/BUILTIN_Users.sql
+++
@@ -1,3 +0,0 @@
-﻿CREATE LOGIN [BUILTIN\Users]
-    FROM WINDOWS WITH DEFAULT_LANGUAGE = [us_english];
-
--- database/Core.Plugins.Database/Security/NT SERVICE_Winmgmt.sql
+++
@@ -1,3 +0,0 @@
-﻿CREATE LOGIN [NT SERVICE\Winmgmt]
-    FROM WINDOWS WITH DEFAULT_LANGUAGE = [us_english];
-
