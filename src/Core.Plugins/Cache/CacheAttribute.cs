fea6aeea5523b5d65101b880bd619e1392d40860
Scott Ferguson <scott.ferguson82@gmail.com>
2020-03-15 14:07:38 -0500

    Adding CacheAttribute

---
+++ src/Core.Plugins/Cache/CacheAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Core.Plugins.Cache
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public sealed class CacheAttribute : Attribute
+    {
+        private double _expirationTime = 120.0D;
+
+        public double ExpirationTime
+        {
+            get { return _expirationTime; }
+            set { _expirationTime = value; }
+        }
+
+        public TimeSpan GetCacheDuration()
+        {
+            return TimeSpan.FromMinutes(ExpirationTime);
+        }
+    }
+}
