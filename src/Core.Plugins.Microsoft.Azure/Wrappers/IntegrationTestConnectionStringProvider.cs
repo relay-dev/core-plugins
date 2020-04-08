16388811afa762112a661d9818839f7f9ba499cb
Scott Ferguson <scott.ferguson82@gmail.com>
2020-04-07 20:15:10 -0500

    Adding IntegrationTestConnectionStringProvider

---
+++ src/Core.Plugins.Microsoft.Azure/Wrappers/IntegrationTestConnectionStringProvider.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Core.Plugins.Microsoft.Azure.Wrappers
+{
+    public class IntegrationTestConnectionStringProvider : AzureConnectionStringByConfigurationProvider
+    {
+        public IntegrationTestConnectionStringProvider(IConfiguration configuration)
+            : base(configuration) { }
+    }
+}
