3289f042b8b536bc0f63f8225da1973455f933a1
Scott Ferguson <scott.ferguson82@gmail.com>
2020-07-10 21:12:27 -0500

    Streamlining some common tasks of dealing with pesky DataTables

---
+++ src/Core.Plugins/Data/Extensions/DataRowExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+
+namespace Core.Plugins.Data.Extensions
+{
+    public static class DataRowExtensions
+    {
+        public static DataPoint DataPoint(this DataRow dataRow, string columnName)
+        {
+            object value = dataRow[columnName];
+
+            return new DataPoint(value);
+        }
+    }
+}
