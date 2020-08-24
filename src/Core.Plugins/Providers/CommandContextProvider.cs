c2da4cff4a3bd60e60174fd701f0d29840ccfee7
Scott Ferguson <scott.ferguson82@gmail.com>
2020-08-23 22:15:41 -0500

    Adding CommandContextProvider

---
+++ src/Core.Plugins/Providers/CommandContextProvider.cs
@@ -0,0 +1,25 @@
+﻿using Core.Framework;
+using Core.Providers;
+
+namespace Core.Plugins.Providers
+{
+    public class CommandContextProvider : ICommandContextProvider
+    {
+        private CommandContext _commandContext;
+
+        public CommandContextProvider()
+        {
+            _commandContext = new CommandContext();
+        }
+
+        public CommandContext Get()
+        {
+            return _commandContext;
+        }
+
+        public void Set(CommandContext context)
+        {
+            _commandContext = context;
+        }
+    }
+}
