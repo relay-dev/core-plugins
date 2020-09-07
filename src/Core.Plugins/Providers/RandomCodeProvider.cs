a395124c223aa511bcaa59a475d7291e92f981f6
Scott Ferguson <scott.ferguson82@gmail.com>
2020-09-07 01:28:28 -0500

    Adding RandomCodeProvider

---
+++ src/Core.Plugins/Providers/RandomCodeProvider.cs
@@ -0,0 +1,43 @@
+﻿using Core.Framework;
+using Core.Providers;
+using System;
+
+namespace Core.Plugins.Providers
+{
+    [Component]
+    [Injectable]
+    public class RandomCodeProvider : IRandomCodeProvider
+    {
+        private readonly IRandomLongProvider _randomLongProvider;
+
+        public RandomCodeProvider(IRandomLongProvider randomLongProvider)
+        {
+            _randomLongProvider = randomLongProvider;
+        }
+
+        public string Get(int length, string prefix = null, string suffix = null)
+        {
+            if (length <= 0)
+            {
+                throw new ArgumentException("length cannot be less than or equal to 0", nameof(length));
+            }
+
+            prefix ??= string.Empty;
+            suffix ??= string.Empty;
+
+            int lengthOfNumericSubstring = length - prefix.Length - suffix.Length;
+
+            if (lengthOfNumericSubstring > 18)
+            {
+                throw new ArgumentException("The numeric portion of the Confirmation Number cannot have a length longer than 18", nameof(length));
+            }
+
+            long minValue = long.Parse("1" + string.Empty.PadRight(lengthOfNumericSubstring - 1, '0'));
+            long maxValue = long.Parse("9" + string.Empty.PadRight(lengthOfNumericSubstring - 1, '9'));
+
+            long randomNumber = _randomLongProvider.Get(minValue, maxValue);
+
+            return $"{prefix}{randomNumber}{suffix}";
+        }
+    }
+}
--- src/Core.Plugins/Providers/RandomLongProvider.cs
+++ src/Core.Plugins/Providers/RandomLongProvider.cs
@@ -25,5 +25,32 @@ namespace Core.Plugins.Providers
 
             return (long)(ulongRand % uRange);
         }
+
+        public long Get(long minValue, long maxValue)
+        {
+            if (minValue < 0)
+                throw new ArgumentException("minValue cannot be less than 0", nameof(minValue));
+
+            if (maxValue <= 0)
+                throw new ArgumentException("maxValue cannot be less than or equal to 0", nameof(maxValue));
+
+            if (maxValue < minValue)
+                throw new ArgumentException("maxValue cannot be less than minValue", nameof(maxValue));
+
+            ulong uRange = (ulong)(maxValue - minValue);
+
+            ulong ulongRand;
+
+            do
+            {
+                byte[] buf = new byte[8];
+
+                new Random(Guid.NewGuid().GetHashCode()).NextBytes(buf);
+
+                ulongRand = (ulong)BitConverter.ToInt64(buf, 0);
+            } while (ulongRand > ulong.MaxValue - ((ulong.MaxValue % uRange) + 1) % uRange);
+
+            return (long)(ulongRand % uRange) + minValue;
+        }
     }
 }
