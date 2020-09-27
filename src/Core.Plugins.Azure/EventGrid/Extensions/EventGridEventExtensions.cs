6209da72981166e7ce4e195c797b8d5ca1ca2a29
Scott Ferguson <scott.ferguson82@gmail.com>
2020-09-26 20:00:55 -0500

    Adding EventGridEvent.GetPayload()

---
+++ src/Core.Plugins.Azure/EventGrid/Extensions/EventGridEventExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Azure.EventGrid.Models;
+using Newtonsoft.Json;
+using System;
+
+namespace Core.Plugins.Azure.EventGrid.Extensions
+{
+    public static class EventGridEventExtensions
+    {
+        public static TPayload GetPayload<TPayload>(this EventGridEvent eventGridEvent)
+        {
+            if (eventGridEvent.Data == null || string.IsNullOrEmpty(eventGridEvent.Data.ToString()))
+            {
+                throw new InvalidOperationException("EventGridEvent.Data property cannot be null or empty");
+            }
+
+            return JsonConvert.DeserializeObject<TPayload>(eventGridEvent.Data.ToString());
+        }
+    }
+}
