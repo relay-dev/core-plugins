1fe10aa458c21adce2b2d90a3572fa2798467b63
Scott Ferguson <scott.ferguson82@gmail.com>
2020-11-17 01:32:03 -0600

    Adding FluentFtp setup to a common location

--- src/Core.Plugins.FileHandling/ServiceCollectionExtensions.cs
+++ src/Core.Plugins.FileHandling/ServiceCollectionExtensions.cs
@@ -4,7 +4,10 @@ using Core.Plugins.FileHandling.Delimited;
 using Core.Plugins.FileHandling.Excel;
 using Core.Plugins.FileHandling.Ftp;
 using Core.Plugins.Ftp;
+using Core.Providers;
+using FluentFTP;
 using Microsoft.Extensions.DependencyInjection;
+using IFtpClient = Core.Ftp.IFtpClient;
 
 namespace Core.Plugins.FileHandling
 {
@@ -12,6 +15,8 @@ namespace Core.Plugins.FileHandling
     {
         public static IServiceCollection AddFileHandlingPlugin(this IServiceCollection services, bool isUseSftp = true)
         {
+            services.AddFluentFtp();
+
             services.AddScoped<IDelimitedFileHandler, GenericParsingDelimitedFileHandler>();
             services.AddScoped<IExcelFileHandler, ClosedXmlExcelHandler>();
             services.AddScoped<IFtpClientFactory, FtpClientFactory>();
@@ -24,6 +29,18 @@ namespace Core.Plugins.FileHandling
             {
                 services.AddScoped<IFtpClient, SystemFtpClient>();
             }
+
+            return services;
+        }
+
+        public static IServiceCollection AddFluentFtp(this IServiceCollection services, string connectionName = "DefaultFtpConnection")
+        {
+            services.AddScoped<FluentFTP.IFtpClient>(sp =>
+            {
+                var settings = new FtpClientSettings(sp.GetRequiredService<IConnectionStringProvider>().Get(connectionName));
+
+                return new FtpClient(settings.Host, settings.Username, settings.Password);
+            });
 
             return services;
         }
