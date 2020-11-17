a0946010ceba13532f591998334266bf7b7d03d7
Scott Ferguson <scott.ferguson82@gmail.com>
2020-11-16 22:52:36 -0600

    Adding AutoMapperHelper

---
+++ src/Core.Plugins.AutoMapper/Helpers/AutoMapperHelper.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using Core.Plugins.AutoMapper.Extensions;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Core.Plugins.AutoMapper.Helpers
+{
+    public static class AutoMapperHelper
+    {
+        public static MapperConfiguration GenerateMapperConfiguration(Assembly[] assemblies)
+        {
+            return new MapperConfiguration(cfg =>
+            {
+                foreach (Assembly assembly in assemblies)
+                {
+                    foreach (Type type in assembly.GetTypes().Where(t => t.BaseType == typeof(Profile)))
+                    {
+                        cfg.AddProfile(type);
+                    }
+                }
+
+                cfg.AddCoreAutoMappers();
+            });
+        }
+    }
+}
