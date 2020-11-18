9c99e563e39fb655d34e372accc7f16ac473795c
Scott Ferguson <scott.ferguson82@gmail.com>
2020-11-18 14:45:17 -0600

    Adding ValidationResultExtensions

---
+++ src/Core.Plugins.FluentValidation/Extensions/ValidationResultExtensions.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using System;
+using System.Linq;
+
+namespace Core.Plugins.FluentValidation.Extensions
+{
+    public static class ValidationResultExtensions
+    {
+        public static void ThrowOnFailure(this ValidationResult validationResult)
+        {
+            if (!validationResult.IsValid)
+            {
+                string errorMessage = $"Validation Failure: The following validation failures occurred: {Environment.NewLine}";
+
+                errorMessage += string.Join(Environment.NewLine, validationResult.Errors.Select(failure => $"'{failure.ErrorMessage}'"));
+
+                throw new ValidationException(errorMessage);
+            }
+        }
+    }
+}
