e14d9f2ffa87f73c297e93e25e37a769b74f91d8
Scott Ferguson <scott.ferguson82@gmail.com>
2020-11-18 15:58:54 -0600

    Let IConfiguration always be easy to find

--- src/Core.Plugins/Configuration/PluginConfiguration.cs
+++ src/Core.Plugins/Configuration/PluginConfiguration.cs
@@ -1,7 +1,11 @@
-﻿namespace Core.Plugins.Configuration
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Core.Plugins.Configuration
 {
     public partial class PluginConfiguration
     {
         public ApplicationConfiguration ApplicationConfiguration { get; set; }
+
+        public IConfiguration Configuration => ApplicationConfiguration.Configuration;
     }
 }
