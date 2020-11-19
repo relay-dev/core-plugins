727a58b961660cea4cd50930a4555a6b7363b86e
Scott Ferguson <scott.ferguson82@gmail.com>
2020-11-19 16:12:19 -0600

    Builder cleanup

--- src/Core.Plugins/Configuration/PluginConfigurationBuilder.cs
+++ src/Core.Plugins/Configuration/PluginConfigurationBuilder.cs
@@ -5,7 +5,7 @@
 
     }
 
-    public partial class PluginConfigurationBuilderGeneric<TBuilder, TResult> : ApplicationConfigurationBuilder<TBuilder, TResult> where TBuilder : class where TResult : class
+    public partial class PluginConfigurationBuilder<TBuilder, TResult> : ApplicationConfigurationBuilder<TBuilder, TResult> where TBuilder : class where TResult : class
     {
         public override TResult Build()
         {
