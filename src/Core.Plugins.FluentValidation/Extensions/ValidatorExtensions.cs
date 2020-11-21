08341dad8c1aeab370d0945b06c27776dfef9eb2
Scott Ferguson <scott.ferguson82@gmail.com>
2020-11-21 16:49:29 -0600

    Adding a validator extension to throw on failure

---
+++ src/Core.Plugins.FluentValidation/Extensions/ValidatorExtensions.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Core.Plugins.FluentValidation.Extensions
+{
+    public static class ValidatorExtensions
+    {
+        public static async Task ValidateAndThrowAsync<TToValidate>(this IValidator<TToValidate> validator, TToValidate instance, CancellationToken cancellationToken)
+        {
+            ValidationResult result = await validator.ValidateAsync(instance, cancellationToken);
+
+            result.ThrowOnFailure();
+        }
+    }
+}
