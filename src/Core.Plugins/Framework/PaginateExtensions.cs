1999268e94e902b2a439345d62462b1d953b172c
Scott Ferguson <scott.ferguson82@gmail.com>
2020-11-29 18:45:20 -0600

    Adding PaginateExtensions

---
+++ src/Core.Plugins/Framework/PaginateExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Core.Framework;
+
+namespace Core.Plugins.Framework
+{
+    public static class PaginateExtensions
+    {
+        public static int Skip(this IPaginate paginate, int defaultPageSize = 100)
+        {
+            return (paginate.PageNumber.GetValueOrDefault() - 1) * paginate.PageSize ?? defaultPageSize;
+        }
+
+        public static int Take(this IPaginate paginate, int defaultPageSize = 100)
+        {
+            return paginate.PageSize ?? defaultPageSize;
+        }
+    }
+}
