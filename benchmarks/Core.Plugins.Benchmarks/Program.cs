4204bd8d63cdebdf6a70c8adbfe25d9faddd92b2
Scott Ferguson <scott.ferguson82@gmail.com>
2020-12-11 15:24:51 -0600

    Adding benchmarks

---
+++ benchmarks/Core.Plugins.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Core.Plugins.Benchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+        }
+    }
+}
---
+++ benchmarks/Core.Plugins.Benchmarks/Security/HashingServiceBenchmarks.cs
@@ -0,0 +1,25 @@
+﻿using BenchmarkDotNet.Attributes;
+using Core.Plugins.Security;
+using System;
+
+namespace Core.Plugins.Benchmarks.Security
+{
+    [HtmlExporter]
+    public class HashingServiceBenchmarks
+    {
+        private readonly Pbkdf2HashingService _pbkdf2HashingService;
+        private readonly Sha256HashingService _sha256HashingService;
+
+        public HashingServiceBenchmarks()
+        {
+            _pbkdf2HashingService = new Pbkdf2HashingService();
+            _sha256HashingService = new Sha256HashingService();
+        }
+
+        [Benchmark]
+        public string Sha256() => _sha256HashingService.CreateHash(Guid.NewGuid().ToString());
+
+        [Benchmark]
+        public string Pbkdf2() => _pbkdf2HashingService.CreateHash(Guid.NewGuid().ToString());
+    }
+}
